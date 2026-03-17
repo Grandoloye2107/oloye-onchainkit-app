5da13bb28ed89ff9cd9ee3274f6c6b0bddfd3dab
grandoloye2107 <granddexterityalluminium@gmail.com>
2026-03-17 18:38:16 -0400

    feat: add filter by collection dropdown

---
+++ CollectionFilter.tsx
@@ -0,0 +1,8 @@
+// Filter NFTs by collection
+export default function CollectionFilter() {
+  return (
+    <select className="border rounded p-2 text-sm">
+      <option>All collections</option>
+    </select>
+  );
+}
