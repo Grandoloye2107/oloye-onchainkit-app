039b9fd50d91cd2b090724b12546ff0cad302120
grandoloye2107 <granddexterityalluminium@gmail.com>
2026-03-17 18:30:36 -0400

    feat: show NFT traits in detail view

--- NFTModal.tsx
+++ NFTModal.tsx
@@ -3,3 +3,4 @@ export default function NFTModal({ open }: { open: boolean }) {
   if (!open) return null;
   return <div className="fixed inset-0 bg-black/50 flex items-center justify-center">NFT Detail</div>;
 }
+// traits display section` comment.
