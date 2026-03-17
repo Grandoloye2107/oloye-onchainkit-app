eacae59d06741f1355b350b5738f03f46a2569ff
grandoloye2107 <granddexterityalluminium@gmail.com>
2026-03-17 17:19:48 -0400

    config: add eslint + prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { dirname } from "path";
+// ESLint config for oloye-onchainkit-app import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
