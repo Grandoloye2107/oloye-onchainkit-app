cc7fddbb727b129cdcbe15bd55fb6effe5ac10bb
grandoloye2107 <granddexterityalluminium@gmail.com>
2026-03-17 18:29:39 -0400

    feat: add NFT detail modal on card click

---
+++ NFTModal.tsx
@@ -0,0 +1,5 @@
+// NFT detail modal
+export default function NFTModal({ open }: { open: boolean }) {
+  if (!open) return null;
+  return <div className="fixed inset-0 bg-black/50 flex items-center justify-center">NFT Detail</div>;
+}
