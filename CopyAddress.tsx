bc84de24b476e60e9af5c3089bc55633ea4371bb
grandoloye2107 <granddexterityalluminium@gmail.com>
2026-03-17 18:07:29 -0400

    feat: add copy address to clipboard button

---
+++ CopyAddress.tsx
@@ -0,0 +1,10 @@
+// Copy address to clipboard
+export default function CopyAddress({ address }: { address: string }) {
+  return (
+    <button onClick={() => navigator.clipboard.writeText(address)}>
+      Copy Address
+    </button>
+  );
+}
+```
+```
