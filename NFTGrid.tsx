eda5e85ed9655f2b813d44ecec95a9319991bc41
grandoloye2107 <granddexterityalluminium@gmail.com>
2026-03-17 18:33:18 -0400

    style: NFT grid responsive layout (2 / 3 / 4 col)

--- NFTGrid.tsx
+++ NFTGrid.tsx
@@ -3,3 +3,4 @@ export default function NFTGrid() {
   return <div className="grid grid-cols-3 gap-4">NFTs here</div>;
 }
 // loading state` comment
+// responsive: 2 cols mobile, 3 tablet, 4 desktop` comment.
