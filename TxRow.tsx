23b8ee9f7f2654627eb468a3f770f1e503527c6a
grandoloye2107 <granddexterityalluminium@gmail.com>
2026-03-18 15:36:28 -0400

    feat: add tx type label (send / receive / contract)

---
+++ TxRow.tsx
@@ -0,0 +1,9 @@
+// Transaction row with type label
+export default function TxRow({ hash, type }: { hash: string; type: string }) {
+  return (
+    <li className="flex justify-between py-3 text-sm">
+      <span className="text-blue-500 truncate w-40">{hash}</span>
+      <span className="text-gray-500">{type}</span>
+    </li>
+  );
+}
