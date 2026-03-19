20aef8324a6ca108c70b4c805652d2cc5975b9f8
grandoloye2107 <granddexterityalluminium@gmail.com>
2026-03-19 07:01:51 -0400

    feat: add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+// 404 page
+export default function NotFound() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-medium mb-2">404</h1>
+      <p className="text-gray-500">Page not found.</p>
+    </main>
+  );
+}
